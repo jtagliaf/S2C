ca0b29b9eb94ebb7713fa544a91a68bad4b8ef5f
tagliaje <jtagliaf@stud.hs-heilbronn.de>
2016-12-21 11:59:50 +0100

    ImageStream

---
+++ RabbitMQ/ImageStream/consumer.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import numpy as np
+import pika
+import cv2
+from skimage import filters
+
+"""
+Verbindung mit der Rabbit  Queue herstellen, schauen, ob die Queue taks_queue2
+vorhanden ist, wenn nicht wird sie erstellt.
+
+"""
+
+credentials = pika.PlainCredentials('rabbitmq', 'TJ3EzFdWEK3zVJE4')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        'localhost', 5672, '/', credentials))
+
+channel = connection.channel()
+channel.queue_declare(queue='task_queue2', durable=True)
+print(' [*] Waiting for messages. To exit press CTRL+C')
+
+
+"""
+baut aus dem gesendeten String, wieder in ein Numpy Array, welches dann in ein
+png Bild umgewandelt werden kann.
+
+"""
+
+def imageDecode(ch, method, properties, body):
+
+    np_array = np.fromstring(body, np.uint8) # umwandlung string to numpy
+    grayImage = cv2.imdecode(np_array, 0) # nur in graustufen umwandeln
+    edges = filters.sobel(grayImage) # berechnet die Umrisse und stellt sie dar.
+
+    # Fuer Performance test auskommentieren
+    #while (True):
+    #    cv2.imshow('frame', edges)
+    #    if cv2.waitKey(1) & 0xFF == ord('q'):
+    #        break
+    ch.basic_ack(delivery_tag = method.delivery_tag)
+
+"""
+erhaelt nur immer eine Nachricht und geht erst zur naechsten, wenn diese
+bearbeitet wurde.
+
+"""
+
+channel.basic_qos(prefetch_count=1) # jeweils nur eine Nachricht annehmen
+channel.basic_consume(imageDecode, queue='task_queue2') # Funktion wird auf die Queue angewandt
+channel.start_consuming()
---
+++ RabbitMQ/ImageStream/producer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+import pika
+import cv2
+import os
+from skimage import io
+
+"""
+Verbindungsdaten RabbitMQ
+
+"""
+
+credentials = pika.PlainCredentials('rabbitmq', 'TJ3EzFdWEK3zVJE4')
+connection = pika.BlockingConnection(pika.ConnectionParameters(
+        'localhost', 5672, '/', credentials))
+channel = connection.channel()
+channel.queue_declare(queue='task_queue2', durable=True)
+
+"""
+Bild umwandeln / senden
+"""
+
+# setze den Bildpfad
+filename = os.path.join('image.jpg')
+
+# wandle das Bild in ein Numpy Array und speichere es in der Variable
+picture = io.imread(filename)
+
+# wandle das Numpy Array in einen string um ( wird in den Buffer geschrieben)
+img_str = cv2.imencode('.jpg', picture)[1].tostring()
+
+# Setze die Menge an Bildern, welche erstellt werden  sollen
+x = 12000
+
+# verschicke das gebufferte Bild
+while x > 2:
+
+# Verschicke, den String und mache die Nachricht persistent
+    channel.basic_publish(exchange='',
+                          routing_key='task_queue2',
+                          body=img_str,
+                          properties=pika.BasicProperties(
+                             delivery_mode = 2, # make message persistent
+                          ))
+    x = x - 1
+print(" [x] Sent ")
+connection.close()
